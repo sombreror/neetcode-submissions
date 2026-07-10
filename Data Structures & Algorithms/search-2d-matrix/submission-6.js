b3f101fb3cac49431dd1bb83faf7f4d75de2b56c
sombreror <150703551+sombreror@users.noreply.github.com>
2026-07-10 19:20:52 +0200

    Add: search-2d-matrix - submission-6

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-6.js
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {number[][]} matrix
+     * @param {number} target
+     * @return {boolean}
+     */
+    searchMatrix(matrix, target) {
+        const ROWS = matrix.length;
+        const COLS = matrix[0].length;
+
+        let left = 0,
+            right = ROWS * COLS - 1;
+        while (left <= right) {
+            const mid = Math.floor((left + right) / 2);
+            const row = Math.floor(mid / COLS);
+            const col = mid % COLS;
+
+            const value = matrix[row][col];
+            if (value === target) {
+                return true;
+            } else if (value < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return false;
+    }
+}
