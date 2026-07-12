393789efbf8b25609988dd9fcaae367c0f8ce9fe
sombreror <150703551+sombreror@users.noreply.github.com>
2026-07-12 21:39:18 +0200

    Add: is-palindrome - submission-7

---
+++ Data Structures & Algorithms/is-palindrome/submission-7.js
@@ -0,0 +1,26 @@
+class Solution {
+  isAlphanumeric(c) {
+        return (
+            (c >= 'a' && c <= 'z') ||
+            (c >= 'A' && c <= 'Z') ||
+            (c >= '0' && c <= '9')
+        );
+    }
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+        const arr = [];
+        for(let c of s){
+            if(this.isAlphanumeric(c)){
+                arr.push(c.toLocaleLowerCase());
+            }
+        }
+        const filtered = arr.join("");
+        const reversed = arr.reverse().join("");
+        return filtered === reversed;
+    }
+}
+
+
