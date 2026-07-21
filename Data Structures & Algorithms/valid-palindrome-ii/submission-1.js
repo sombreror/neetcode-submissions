c3a2e990cdd17e36cb55c951a2606cf7d64b309b
sombreror <150703551+sombreror@users.noreply.github.com>
2026-07-21 15:18:15 +0200

    Add: valid-palindrome-ii - submission-1

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-1.js
@@ -0,0 +1,37 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    validPalindrome(s) {
+        let left = 0;
+        let right = s.length - 1;
+
+        while (left < right) {
+            if (s[left] !== s[right]) {
+                return (
+                    isPalindrome(s, left + 1, right) ||
+                    isPalindrome(s, left, right - 1)
+                );
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
+
+function isPalindrome(s, left, right) {
+    while (left < right) {
+        if (s[left] !== s[right]) {
+            return false;
+        }
+
+        left++;
+        right--;
+    }
+
+    return true;
+}
