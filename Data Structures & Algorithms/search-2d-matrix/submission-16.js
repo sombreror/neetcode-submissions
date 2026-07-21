41c0184ac25af6741d21e6612679f6c605d9e0cc
sombreror <150703551+sombreror@users.noreply.github.com>
2026-07-21 20:35:52 +0200

    Add: search-2d-matrix - submission-16

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-16.js
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param {number[][]} matrix
+     * @param {number} target
+     * @return {boolean}
+     */
+    searchMatrix(matrix, target) {
+        for (let r = 0; r < matrix.length; r++) {
+            for (let c = 0; c < matrix[r].length; c++) {
+                if (matrix[r][c] == target) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
